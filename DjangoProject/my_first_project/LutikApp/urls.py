58d6d818c8179add986dd7e6c94f04cb07dddae2
Lutik72 <gribova.sofia24@yandex.ru>
2026-03-03 10:06:05 +0300

    Delete DjangoProject/my_first_project directory

--- DjangoProject/my_first_project/LutikApp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views # Импортируем представления из текущего приложения
-
-# Этот список urlpatterns будет подключен к главному urls.py проекта
-urlpatterns = [ # Маршрут для корня приложения
-    path('details/', views.hello, name='myapp_details'),
-]
