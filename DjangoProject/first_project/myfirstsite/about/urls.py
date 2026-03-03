4507e6049e5a6e499b65114bc72bf5381695b403
Sofia <gribova.sofia24@yandex.ru>
2026-03-03 10:53:52 +0300

    :)

--- DjangoProject/first_project/myfirstsite/about/urls.py
+++ DjangoProject/first_project/myfirstsite/about/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.about_me, name='about_me'),           # /about/
-    path('contacts/', views.my_contacts, name='contacts'),  # /about/contacts/
+    path('', views.about_page, name='about_me'),           # /about/
+    # path('contacts/', views.my_contacts, name='contacts'),  # /about/contacts/
 ]
