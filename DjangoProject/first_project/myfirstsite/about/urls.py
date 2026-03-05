2890802cf2d009985d3f4f63ec54fd779398ad67
Sofia <gribova.sofia24@yandex.ru>
2026-03-05 11:10:09 +0300

    обновление шаблонов

--- DjangoProject/first_project/myfirstsite/about/urls.py
+++ DjangoProject/first_project/myfirstsite/about/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.about_page, name='about_me'),           # /about/
+    path('', views.about_page, name='about'),           # /about/
     # path('contacts/', views.my_contacts, name='contacts'),  # /about/contacts/
 ]
--- DjangoProject/first_project/myfirstsite/about/views.py
+++ DjangoProject/first_project/myfirstsite/about/views.py
@@ -4,4 +4,9 @@ from django.shortcuts import render
 
 def about_page(request):
     template = 'about/index.html'
-    return render(request, template)
+    title = "Меню - Уютная кофейня"
+
+    context = {
+        'title': title
+    }
+    return render(request, template, context)
