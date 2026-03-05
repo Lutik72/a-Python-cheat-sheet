c515b1aeb7f76d40aba7d95ce313409deaef2f69
Sofia <gribova.sofia24@yandex.ru>
2026-03-05 12:19:13 +0300

    Обновление настроек

--- DjangoProject/first_project/myfirstsite/myfirstsite/settings.py
+++ DjangoProject/first_project/myfirstsite/myfirstsite/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = "myfirstsite.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
 
@@ -117,6 +117,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-    )
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
