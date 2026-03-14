c8cf2668d54ac749975100e852adcff1fde322c8
Sofia <gribova.sofia24@yandex.ru>
2026-03-14 15:22:33 +0300

    обновление стиля

--- DjangoProject/first_project/myfirstsite/about/views.py
+++ DjangoProject/first_project/myfirstsite/about/views.py
@@ -5,11 +5,9 @@ def about_page(request):
     template = 'about/index.html'
     title = "Меню - Уютная кофейня"
     subtitle = "Вдохновение в каждой чашке"
-    header1 = "☕ Наше меню"
 
     context = {
         'title': title,
-        'header1' : header1,
         "subtitle" : subtitle
     }
     return render(request, template, context)
--- DjangoProject/first_project/myfirstsite/coworking/views.py
+++ DjangoProject/first_project/myfirstsite/coworking/views.py
@@ -5,14 +5,12 @@ from django.shortcuts import render
 def coworking_page(request):
     template = "coworking/index.html"
     title = "Уютная кофейня"
-    header1 = "💻 Коворкинг-зона"
     subtitle = "Коворкинг с ароматом кофе — твой второй офис в сердце города"
     tariffs = Tariff.objects.filter(is_active=True).order_by('order')
   
     context = {
         "title": title,
         "subtitle": subtitle,
-        "header1" : header1,
         'tariffs': tariffs,
     }
     return render(request, template, context)
